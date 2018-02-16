4237d0e6e14bec13f2044caf54cbc42843462346
Krzysztof Krysiak <krysiak86@gmail.com>
2018-02-16 15:08:14 +0100

    Add UserControllerRegisterUserTests / NSubstitude mock library

---
+++ DependencyInjectionTutorial.App.Tests/UsersControllerRegisterUserTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NSubstitute;
+using Xunit;
+
+namespace DependencyInjectionTutorial.App.Tests
+{
+    public class UsersControllerRegisterUserTests
+    {
+        private readonly UsersController _controller;
+        private readonly IEmailValidator _emailValidator;
+        private string _email;
+
+        public UsersControllerRegisterUserTests()
+        {
+            _emailValidator = Substitute.For<IEmailValidator>();
+            var linkGenerator = Substitute.For<IActivationLinkGenerator>();
+            _controller = new UsersController(_emailValidator, linkGenerator);
+            _email = "email";
+        }
+
+        private void Execute()
+        {
+            _controller.RegisterUser(_email);
+        }
+
+        [Fact]
+        public void ThrowsWhenEmailNotValid()
+        {
+            _emailValidator.Validate(_email).Returns(false);
+            Assert.Throws<ArgumentException>(() => Execute());
+        }
+    }
+}
