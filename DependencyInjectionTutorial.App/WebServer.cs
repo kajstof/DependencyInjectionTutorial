c0da1311c67ae17111ead87095c18f148db2769c
Krzysztof Krysiak <krysiak86@gmail.com>
2018-02-19 22:07:39 +0100

    Added own DI

--- DependencyInjectionTutorial.App/WebServer.cs
+++ DependencyInjectionTutorial.App/WebServer.cs
@@ -2,12 +2,32 @@
 {
     public class WebServer
     {
+        private static PoorMansContainer _container;
+
+        static void Main()
+        {
+            _container = new PoorMansContainer();
+            _container.Register<IEmailValidator, EmailValidator>();
+            _container.Register<IActivationLinkGenerator, ActivationLinkGenerator>();
+            _container.Register<IEmailService, EmailService>();
+            // Application compiles, but will throw in runtime
+            // container.Register<IEmailTemplateGenerator, ???>();
+            _container.RegisterType<UsersController>();
+        }
+
+        static void Shutdown()
+        {
+            // Our container does not implement this
+            // _container.Dispose();
+        }
+
         public void RegisterUser(string email)
         {
-            var emailService = new EmailService();
-            var emailValidator = new EmailValidator();
-            var activationLinkGenerator = new ActivationLinkGenerator();
-            var controller = new UsersController(emailService, emailValidator, activationLinkGenerator);
+//            var emailService = new EmailService(new IEmailTemplateGenerator());
+//            var emailValidator = new EmailValidator();
+//            var activationLinkGenerator = new ActivationLinkGenerator();
+//            var controller = new UsersController(emailService, emailValidator, activationLinkGenerator);
+            var controller = _container.Resolve<UsersController>();
             controller.RegisterUser(email);
         }
     }
