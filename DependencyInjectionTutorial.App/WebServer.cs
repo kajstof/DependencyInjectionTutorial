a3ccf04c8898f057e4324507599ebf810b5bd915
Krzysztof Krysiak <krysiak86@gmail.com>
2018-02-19 22:31:10 +0100

    Add Autofac as DI Container

--- DependencyInjectionTutorial.App/WebServer.cs
+++ DependencyInjectionTutorial.App/WebServer.cs
@@ -1,32 +1,36 @@
-﻿namespace DependencyInjectionTutorial.App
+﻿using System.Reflection;
+using Autofac;
+
+namespace DependencyInjectionTutorial.App
 {
     public class WebServer
     {
-        private static PoorMansContainer _container;
+        private static IContainer _container;
 
         static void Main()
         {
-            _container = new PoorMansContainer();
-            _container.Register<IEmailValidator, EmailValidator>();
-            _container.Register<IActivationLinkGenerator, ActivationLinkGenerator>();
-            _container.Register<IEmailService, EmailService>();
-            // Application compiles, but will throw in runtime
-            // container.Register<IEmailTemplateGenerator, ???>();
-            _container.RegisterType<UsersController>();
+            var builder = new ContainerBuilder();
+            Assembly executingAssembly = Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(executingAssembly)
+                .AsSelf()
+                .AsImplementedInterfaces();
+            
+            // Override default registration if needed
+            builder.RegisterType<EmailValidator>()
+                .AsImplementedInterfaces()
+                .SingleInstance();
+
+            _container = builder.Build();
         }
 
         static void Shutdown()
         {
-            // Our container does not implement this
-            // _container.Dispose();
+            _container.Dispose();
         }
 
         public void RegisterUser(string email)
         {
-//            var emailService = new EmailService(new IEmailTemplateGenerator());
-//            var emailValidator = new EmailValidator();
-//            var activationLinkGenerator = new ActivationLinkGenerator();
-//            var controller = new UsersController(emailService, emailValidator, activationLinkGenerator);
             var controller = _container.Resolve<UsersController>();
             controller.RegisterUser(email);
         }
